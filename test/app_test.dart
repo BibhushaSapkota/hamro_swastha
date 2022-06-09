dd13d929b2c54377bf1ef6d822dbcc5d988a4a40
BibhushaSapkota <Bibhushasapkota5@gmail.com>
2022-06-09 18:38:46 +0545

    commits

---
+++ test/app_test.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:mero_doctor/main.dart';
+
+void main() {
+  testWidgets("testing ", (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    expect(find.byType(MaterialApp), findsOneWidget);
+  });
+}
