871be486cb29c565fa4865605479e8bf926fe841
BibhushaSapkota <Bibhushasapkota5@gmail.com>
2022-06-09 18:49:25 +0545

    commits

--- test/app_test.dart
+++ test/app_test.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:mero_doctor/main.dart';
+import 'package:mero_doctor/screens/choose_screen.dart';
 
 void main() {
   testWidgets("testing ", (WidgetTester tester) async {
@@ -9,4 +10,16 @@ void main() {
 
     expect(find.byType(MaterialApp), findsOneWidget);
   });
+
+  testWidgets("testing choosing screen ", (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: ChooseScreen()));
+    expect(find.text("Who are you?"), findsOneWidget);
+  });
+
+  testWidgets("testing choosing screen 2 ", (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home: ChooseScreen()));
+    expect(find.text("Doctor"), findsOneWidget);
+  });
 }
+
+
