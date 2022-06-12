9defbb6d34f75a5d57ccb1b2657dd0fcbc1fa9ef
BibhushaSapkota <Bibhushasapkota5@gmail.com>
2022-06-12 18:59:49 +0545

    commit

--- test/sprint5_test.dart
+++ test/sprint5_test.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:mero_doctor/main.dart';
 import 'package:mero_doctor/screens/splash_screen.dart';
 
 void main() {
-  testWidgets("testing ", (WidgetTester tester) async {
+  testWidgets("testing material app ", (WidgetTester tester) async {
     await tester.pumpWidget(MyApp());
 
     expect(find.byType(MaterialApp), findsOneWidget);
