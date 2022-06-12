36425dff3396e2e0c5dd2dc366ef7a81c85cafea
BibhushaSapkota <Bibhushasapkota5@gmail.com>
2022-06-12 18:55:57 +0545

    test commit

---
+++ test/sprint5_test.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:mero_doctor/main.dart';
+import 'package:mero_doctor/screens/splash_screen.dart';
+
+void main() {
+  testWidgets("testing ", (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+
+    expect(find.byType(MaterialApp), findsOneWidget);
+  });
+
+  testWidgets("testing Splash Screen ", (WidgetTester tester) async {
+    await tester.pumpWidget(MaterialApp(home:SplashScreen()));
+
+    expect(find.text("Powered by Mero Health"), findsOneWidget);
+  });
+
+
+}
